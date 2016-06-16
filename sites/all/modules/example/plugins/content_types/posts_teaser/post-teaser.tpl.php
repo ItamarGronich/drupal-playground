68aad711014bce7901f0a8200bf74183e97e100d
Itamar Gronich <itamar.gronich@gizra.com>
2016-06-16 15:07:47 +0300

    Initial commit

---
+++ sites/all/modules/example/plugins/content_types/posts_teaser/post-teaser.tpl.php
@@ -0,0 +1,9 @@
+<div >
+  <div>
+    <div>
+      <div><?php print $image; ?></div>
+      <?php print $title; ?>    </div>
+    <p><?php print $body; ?>
+    </p>
+  </div>
+</div>
---
+++ sites/all/themes/Itamars_theme/page.tpl.php
@@ -0,0 +1,28 @@
+
+<?php if ($page['header']): ?>
+  <header id="header">
+
+    <?php print render($page['header']); ?>
+  </header>
+<?php endif; ?>
+
+
+<section id="wrapper">
+
+  <main id="content">
+    <?php print render($page['content']); ?>
+  </main>
+
+  <?php if ($page['sidebar_first']): ?>
+    <aside id="sidebar_first">
+      <?php print render($page['sidebar_first']); ?>
+    </aside>
+  <?php endif; ?>
+
+</section>
+
+<?php if ($page['footer']): ?>
+  <div id="footer">
+    <?php print render($page['footer']); ?>
+  </div>
+<?php endif; ?>
